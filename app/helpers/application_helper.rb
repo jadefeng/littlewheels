5c9a65323a9e39889d97efe1aa67d04e8395671b
jadefeng <j.feng.1991@gmail.com>
2014-10-03 18:04:49 +1000

    Links are all working. Basic structure is up. Let's goooo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def intellinav
 		nav = ''
 		if @current_user.present?
-			nav += "<li> Hello #{@current_user.name} </li> "
+			nav += "<li> Hello #{@current_user.username} </li> "
 			nav += "<li>" + link_to('All users', users_path) + "</li> "
 			nav += "<li>" + link_to('Edit user', edit_user_path) + "</li> "
 			nav += "<li> #{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => "Are you sure?"})  }</li> "
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
 	belongs_to :suburb
 	has_many :kids
 	has_many :trips
+
+	validates :username, :presence => true, :uniqueness => true, :length => {:minimum => 2, :maximum => 20}
+	validate :email, :presence => true, :uniqueness => true
+
 end
