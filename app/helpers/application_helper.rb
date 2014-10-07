bf9ae784d8eeda8318c527f16f9d6d7b4247c6d4
jadefeng <j.feng.1991@gmail.com>
2014-10-07 19:06:18 +1100

    Adding a permanent nav bar on each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,34 @@ module ApplicationHelper
 	def intellinav
 		nav = ''
 		if @current_user.present?
-			nav += "<li>" + link_to('All users', users_path) + "</li> "
-			nav += "<li>" + link_to('Edit user', edit_user_path) + "</li> "
-			nav += "<li>" + link_to('All Kids', kids_path) + "</li> "
-			nav += "<li>" + link_to('Register A Kid', new_kid_path) + "</li> "
-			nav += "<li>" + link_to('All Trips', trips_path) + "</li> "
-			nav += "<li>" + link_to('Register A Trip', new_trip_path) + "</li> "
+			nav += "<li>" + link_to('All Profiles', users_path) + "</li> "
+			nav += "<li>" + link_to('Edit Profile', edit_user_path) + "</li> "
+			nav += "<li>" + link_to('All Children', kids_path) + "</li> "
+			nav += "<li>" + link_to('Register A Student', new_kid_path) + "</li> "
+			nav += "<li>" + link_to('All Rides', trips_path) + "</li> "
+			nav += "<li>" + link_to('Register A Ride', new_trip_path) + "</li> "
 			nav += "<li>" + link_to('All Schools', schools_path) + "</li> "
 			nav += "<li>" + link_to('Register A School', new_school_path) + "</li> "
 			nav += "<li>  #{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => "Are you sure?"})  }</li> "
+			# nav += #{ <a href='/users/" + <%= @current_user.id %> + "'> <img src=" + <%= @current_user.image %> + "class='profile_icon' > </a>"	}			
+		else 
+			nav += "<li> #{ link_to('Sign Up', new_user_path) }</li> "
+			nav += "<li> #{ link_to('Login', login_path) } </li> "
+		end 
+		nav
+	end
+
+	def intellinav_short
+		nav = ''
+		if @current_user.present?
+			nav += "<li>" + link_to('Home', root_path) + "</li> "
+			nav += "<li>" + link_to('All Profiles', users_path) + "</li> "
+			nav += "<li>" + link_to('All Children', kids_path) + "</li> "
+			nav += "<li>" + link_to('All Rides', trips_path) + "</li> "
+			nav += "<li>" + link_to('All Schools', schools_path) + "</li> "
+			nav += "<li>" + link_to('About', about_path) + "</li> "
+			nav += "<li>" + link_to('Contact', contact_path) + "</li> "
+
 			# nav += #{ <a href='/users/" + <%= @current_user.id %> + "'> <img src=" + <%= @current_user.image %> + "class='profile_icon' > </a>"	}			
 		else 
 			nav += "<li> #{ link_to('Sign Up', new_user_path) }</li> "
